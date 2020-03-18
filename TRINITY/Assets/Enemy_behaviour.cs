46777463da5c748787dcc52b5dbf411ed08f1452
Marti <marti.mayolas@enti.cat>
2020-03-18 18:45:23 +0100

    Enemic Esquelet

---
+++ TRINITY/Assets/Enemy_behaviour.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_behaviour : MonoBehaviour
+{
+
+    #region Public Variables
+    public Transform rayCast;
+    public LayerMask raycastMask;
+    public float rayCastLength;
+    public float attackDistance; //Minimum distance for attack
+    public float moveSpeed;
+    public float timer; // Timer for cooldown between attacks
+
+    #endregion
+
+    #region Private Variables
+    private RaycastHit2D hit;
+    private GameObject target;
+    private Animator anim;
+    private float distance; //Store the distance between enemy and player
+    private bool attackMode;
+    private bool inRange; // Check if Plalyer is in range
+    private bool cooling; // Check if Enemy is cooling after attack
+    private float intTimer;
+    #endregion
+
+    void Awake()
+    {
+        intTimer = timer; //Store the initial value fo timer
+        anim = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (inRange)
+        {
+            hit = Physics2D.Raycast(rayCast.position, Vector2.left, rayCastLength, raycastMask);
+            RaycastDebugger();
+        }
+
+        //When PLayer is detected
+
+        if(hit.collider != null)
+        {
+            EnemyLogic();
+
+        }else if(hit.collider == null)
+        {
+            inRange = false;
+        }
+        if(inRange == false)
+        {
+            anim.SetBool("canWalk", false);
+            StopAttack();
+        }
+
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D trig)
+    {
+        if(trig.gameObject.tag == "Players")
+        {
+            target = trig.gameObject;
+            inRange = true;
+        }
+    }
+
+    void EnemyLogic()
+    {
+        distance = Vector2.Distance(transform.position, target.transform.position);
+
+        if (distance > attackDistance) { 
+        Move();
+        StopAttack();
+        }
+        else if(attackDistance >= distance && cooling == false)
+        {
+            Attack();
+        }
+
+        if (cooling)
+        {
+            CoolDown();
+            anim.SetBool("Attack", false);
+        }
+    }
+
+   void Move()
+    {
+        anim.SetBool("canWalk", true);
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Skeleton_attack"))
+        {
+            Vector2 targetPosition = new Vector2(target.transform.position.x, transform.position.y);
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+    }
+
+    void Attack()
+    {
+        timer = intTimer; // Reset Timer when PLayer enter Attack Range
+        attackMode = true; // To check if Enemy can still attack of not
+
+        anim.SetBool("CanWalk", false); 
+        anim.SetBool("Attack", true); 
+
+    }
+
+    void CoolDown()
+    {
+
+        timer -= Time.deltaTime;
+
+        if(timer <= 0 && cooling && attackMode)
+        {
+            cooling = false;
+            timer = intTimer;
+        }
+
+
+    }
+    
+    void StopAttack()
+    {
+        cooling = false;
+        attackMode = false;
+        anim.SetBool("Attack", false);
+    }
+
+    void RaycastDebugger()
+    {
+        if(distance > attackDistance)
+        {
+            Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.red);
+
+        }else if(attackDistance > distance)
+        {
+            Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.green);
+        }
+
+    }
+
+    public void TriggerCooling()
+    {
+        cooling = true;
+    }
+    
+
+
+}
